6feb749d89617b440b530b15d33564beeafda1e5
Amith E K <135840620+amithhhh@users.noreply.github.com>
2025-07-05 07:39:02 +0000

    Initialize micro-loan contract

--- micro_loan_platform/src/lib.rs
+++ micro_loan_platform/src/lib.rs
@@ -1,3 +1,17 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
 use soroban_sdk::{contract, contractimpl, Env, Address, Vec, log};
 use serde::{Deserialize, Serialize};
 
